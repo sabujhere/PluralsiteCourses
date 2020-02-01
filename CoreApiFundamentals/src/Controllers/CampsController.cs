d72ab6a129ac0198b75128471d97184e6aafc4fb
sabujhere <sabujhere@users.noreply.github.com>
2020-02-01 08:55:10 -0600

    added camps controller

---
+++ CoreApiFundamentals/src/Controllers/CampsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository _repository;
+
+        public CampsController(ICampRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _repository.GetAllCampsAsync();
+                return Ok(results);
+            }
+            catch (Exception e)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failed");
+            }
+        }
+
+    }
+}
