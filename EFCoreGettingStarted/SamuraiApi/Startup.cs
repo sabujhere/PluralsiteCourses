31a9a9935b47bd5eaf6605a19c6c1c0600aa94d2
sabuj.saha@sandc.com <sabuj.saha@sandc.com>
2020-04-11 23:06:00 -0500

    hooked up the asp.net core project

--- EFCoreGettingStarted/SamuraiApi/Startup.cs
+++ EFCoreGettingStarted/SamuraiApi/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SamuraiApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace SamuraiApi
 {
@@ -25,6 +27,8 @@ namespace SamuraiApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<SamuraiContext>(opt=> 
+                opt.UseSqlServer(Configuration.GetConnectionString("SamuraiConnex")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- EFCoreGettingStarted/SamuraiApp.Data/SamuraiContext.cs
+++ EFCoreGettingStarted/SamuraiApp.Data/SamuraiContext.cs
@@ -7,29 +7,24 @@ using Microsoft.Extensions.Logging;
 
 namespace SamuraiApp.Data
 {
-    public class SamuraiContext : DbContext
+    public sealed class SamuraiContext : DbContext
     {
+        //needed for the console app
+        public SamuraiContext()
+        {
+            
+        }
+
+        public SamuraiContext(DbContextOptions<SamuraiContext> options)
+        :base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
         public DbSet<Battle> Battles { get; set; }
 
-        public static readonly ILoggerFactory ConsoleLoggerFactory
-            = LoggerFactory.Create(builder =>
-            {
-                builder
-                    .AddFilter((category, level) =>
-                        category == DbLoggerCategory.Database.Command.Name
-                        && level == LogLevel.Information)
-                    .AddConsole();
-            });
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder
-                .UseLoggerFactory(ConsoleLoggerFactory)
-                .UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
