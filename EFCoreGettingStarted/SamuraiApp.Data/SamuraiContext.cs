f77c395dcfc84129ffdf1ba483fed40d9890910c
sabuj.saha@sandc.com <sabuj.saha@sandc.com>
2020-04-11 21:58:01 -0500

    added logging

--- EFCoreGettingStarted/SamuraiApp.Data/SamuraiContext.cs
+++ EFCoreGettingStarted/SamuraiApp.Data/SamuraiContext.cs
@@ -3,6 +3,7 @@ using SamuraiApp.Domain;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Extensions.Logging;
 
 namespace SamuraiApp.Data
 {
@@ -11,10 +12,23 @@ namespace SamuraiApp.Data
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        public static readonly ILoggerFactory ConsoleLoggerFactory
+            = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter((category, level) =>
+                        category == DbLoggerCategory.Database.Command.Name
+                        && level == LogLevel.Information)
+                    .AddConsole();
+            });
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
+            optionsBuilder
+                .UseLoggerFactory(ConsoleLoggerFactory)
+                .UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
